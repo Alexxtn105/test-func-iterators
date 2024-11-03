70041c612712a414bba4c357cf5154041e6fde70
Alexxtn105 <netmann105@gmail.com>
2024-11-03 16:54:17 +0300

    Добавил функцию-итератор для слайсов

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"iter"
 )
 
+//region Простейший пример
+
 // countTo Пример простейшей функции-итератора. Возвращаем одно значение типа int (iter.Seq[int])
 func countTo(s int) iter.Seq[int] {
 	// функция-итератор должна ПРИ КАЖДОЙ ИТЕРАЦИИ возвращать такое значение:
@@ -17,14 +19,48 @@ func countTo(s int) iter.Seq[int] {
 				return
 			}
 		}
+	}
+}
+
+//endregion
+
+// region Пример со слайсами (с дженериками)
+
+// Backwards Функция обратного перебора элементов слайса
+func Backwards[V any](s []V) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		// пишем собственную реализацию итератора
+		for i := len(s) - 1; i >= 0; i-- {
+			if !yield(s[i]) {
+				return
+			}
+		}
 
 	}
 }
 
+//endregion
+
 func main() {
-	//nums := []int{0, 1, 2, 3, 4, 5}
-	// вызов функции-итератора (countTo) в рамках range
+	//region Вызов простейшей функции-итератора (countTo) в рамках range
+	fmt.Println("---------------------------------------------")
+	fmt.Println("Вывод простейшей функции-итератора - countTo:")
+	// вызываем функцию-итератор
 	for v := range countTo(5) {
 		fmt.Println(v)
 	}
+	//endregion
+
+	//region Вызов функции-итератора для слайса
+	fmt.Println("----------------------------------------------")
+	fmt.Println("Вывод функции-итератора для слайса - Backwards")
+	nums := []int{0, 1, 2, 3, 4, 5}
+	// 5, 4, 3, 2, 1, 0
+	// вызываем функцию-итератор для слайса
+	for v := range Backwards(nums) {
+		fmt.Println(v)
+	}
+
+	//endregion
+
 }
